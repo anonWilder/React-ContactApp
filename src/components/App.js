c47abf8fb27a4745aaffff7b78a36f86d295bc55
anonWilder <austineokafor.e@gmail.com>
2023-07-08 19:19:18 +0100

    imported the components into the App.js which is passed into the index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact'
+import ContactList from './ContactList'
 import './App.css';
 
 function App() {
   return (
-    div
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
